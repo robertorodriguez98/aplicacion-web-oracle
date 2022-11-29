a645c3c2941ef34b7aad7fbd2dfe364e86022ccb
Roberto Rodríguez Márquez <krowno.iso@gmail.com>
2022-11-29 12:13:26 +0100

    cambios menores

--- aplicacion.py
+++ aplicacion.py
@@ -1,61 +1,40 @@
 import os
 import sys
 import cx_Oracle
-from flask import Flask
-
-def init_session(connection, requestedTag_ignored):
-    cursor = connection.cursor()
-    cursor.execute("""
-        ALTER SESSION SET
-          TIME_ZONE = 'UTC'
-          NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI'""")
-
-# start_pool(): starts the connection pool
-def start_pool():
-    pool_min = 4
-    pool_max = 4
-    pool_inc = 0
-    pool_gmd = cx_Oracle.SPOOL_ATTRVAL_WAIT
-
-    print("Connecting to", "192.168.122.105:1521/ORCLCDB")
-
-    pool = cx_Oracle.SessionPool(user="roberto",
-                                 password="roberto",
-                                 dsn="192.168.122.105:1521/ORCLCDB",
-                                 min=pool_min,
-                                 max=pool_max,
-                                 increment=pool_inc,
-                                 threaded=True,
-                                 getmode=pool_gmd,
-                                 sessionCallback=init_session)
-
-    return pool
+from flask import Flask, render_template, request,abort
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Entra en /emp/nombreempleado para ver los datos del empleado"
+def inicio():
+    return render_template("inicio.html")
+
+@app.route('/consulta',methods=["POST"])
+def iniciosesion():
+    usuario=request.form['usuario']
+    contra=request.form['contra']
+    while True:
+        try:
+            connection = cx_Oracle.connect(
+            user=usuario,
+            password=contra,
+            dsn="192.168.122.204:1521/ORCLCDB")
+            cursor = connection.cursor()
+            cursor.execute("select * from emp")
+            res = cursor.fetchall()        
+            return render_template("consulta.html", res=res)
+        except cx_Oracle.DatabaseError:
+            return render_template("error.html")
+    
+
+    #return(res)
 
 
-# Show the username for a given id
-@app.route('/emp/<string:name>')
-def show_username(name):
-    connection = pool.acquire()
-    cursor = connection.cursor()
-    cursor.execute("select * from emp where ename = (:name)", [name])
-    r = cursor.fetchone()
-    r = str(r)
-    return (r)
 
-################################################################################
-#
-# Initialization is done once at startup time
-#
 if __name__ == '__main__':
 
     # Start a pool of connections
-    pool = start_pool()
+    #pool = start_pool()
 
 
     # Start a webserver
